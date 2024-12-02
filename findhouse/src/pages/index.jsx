698b3ef3da211d495909915fdf3d2d2671a9d5b5
jacky7584 <44133270+jacky7584@users.noreply.github.com>
2024-12-02 22:59:35 +0800

    feat: new line

--- findhouse/src/pages/index.jsx
+++ findhouse/src/pages/index.jsx
@@ -1,16 +1,21 @@
 'use client'; // 標識這是 Client Component
 
 import Image from "next/image";
-import { useState } from 'react';
 
 export default function Home() {
-  const [loginUrl, setLoginUrl] = useState('');
+  const LINE_LOGIN_URL = "https://access.line.me/oauth2/v2.1/authorize";
+  const LINE_CHANNEL_ID = process.env.NEXT_PUBLIC_LINE_CLIENT_ID;
 
-  const handleLogin = async () => {
-    const response = await fetch('/api/login');
-    const data = await response.json();
-    setLoginUrl(data.loginUrl);
-    window.location.href = data.loginUrl; // 跳轉到 LINE Login
+  const handleLoginWithLine = () => {
+    const params = new URLSearchParams({
+      response_type: "code",
+      client_id: LINE_CHANNEL_ID,
+      redirect_uri: process.env.NEXT_PUBLIC_LINE_REDIRECT_URI,
+      state: "random-state-value",
+      scope: "profile openid email",
+    });
+  
+    window.location.href = `${LINE_LOGIN_URL}?${params.toString()}`;
   };
 
   return (
@@ -25,7 +30,7 @@ export default function Home() {
       }}>
         <button
           className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#00c300] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-          onClick={handleLogin}
+          onClick={handleLoginWithLine}
         >
           <Image
             className="dark:invert"
